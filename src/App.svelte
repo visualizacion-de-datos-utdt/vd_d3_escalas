c9c5406803045b6033469b5406af2fe90fd70ec3
Carlos Araujo <infocarlosaraujo@gmail.com>
2024-08-28 01:26:48 -0300

    chore: Refactor code formatting in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,54 @@
 <!-- Script JS -->
 <script>
-    /* Importamos d3 para tenerlo disponible */
-  import * as d3 from 'd3'
+  /* Importamos d3 para tenerlo disponible */
+  import * as d3 from "d3"
 
   let data = [1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
 
-  let altura = d3.scaleLinear()
+  let altura = d3
+    .scaleLinear()
     .domain([0, d3.max(data)])
     .range([0, 400])
 
-  let colorLightness = d3.scaleLinear()
+  let colorLightness = d3
+    .scaleLinear()
     .domain([0, d3.max(data)])
-    .range(['white', '#EE0001'])
-
+    .range(["white", "#EE0001"])
 </script>
 
 <!-- Estructura contenido HTML -->
 <h2>Ejemplo 1: Altura</h2>
 <div class="container">
-  <!-- <div class="column_wrapper">
+  <!-- <div class="item_wrapper">
     <p class="num">1000</p>
     <div class="column"></div> 
     </div>-->
-    {#each data as num}
-    <div class="column_wrapper">
+  {#each data as num}
+    <div class="item_wrapper">
       <p class="num">{num}</p>
       <div class="column" style="height: {altura(num)}px"></div>
     </div>
-    {/each}
-    <!-- </div> -->
-  </div>
-  <br>
-  <br>
+  {/each}
+  <!-- </div> -->
+</div>
+<br />
+<br />
 
-  <h2>Ejemplo 2: Color</h2>
-  <div class="container_2">
-    {#each data as num}
-    <div class="circle" style="background-color: {colorLightness(num)}"></div>
-    {/each}
-  </div>
-
+<h2>Ejemplo 2: Color</h2>
+<div class="container_2">
+  {#each data as num}
+    <div class="item_wrapper">
+      <div class="circle" style="background-color: {colorLightness(num)}"></div>
+      <p class="num_2">{num}</p>
+    </div>
+  {/each}
+</div>
 
 <!-- Estilos CSS -->
 <style>
   /* Estilos CSS */
-  .container, .container_2 {
+  .container,
+  .container_2 {
     display: flex;
     justify-content: space-between;
     gap: 10px;
@@ -52,10 +56,10 @@
     border: 1px solid #000;
   }
   .container_2 {
-    height: 80px;
+    height: auto;
   }
 
-  .column_wrapper {
+  .item_wrapper {
     display: flex;
     flex-direction: column;
     justify-content: end;
@@ -65,20 +69,23 @@
   .column {
     width: 80px;
     height: 100%;
-    background-color: #EE0001;
+    background-color: #ee0001;
   }
   .num {
     font-size: 21px;
     font-weight: bold;
     margin: 0;
-    color: #EE0001;
+    color: #ee0001;
+  }
+  .num_2 {
+    font-size: 18px;
+    margin: 0;
+    color: #ee0001;
   }
   .circle {
     width: 80px;
     height: 80px;
     border-radius: 50%;
-    background-color: #EE0001;
+    background-color: #ee0001;
   }
-
-  
 </style>
