e17688db48b55e4cedea740d491b4cd7c636395e
Carlos Araujo <infocarlosaraujo@gmail.com>
2024-08-29 11:40:42 -0300

    feat: add concentric circles with different radii to circles.svg

--- src/App.svelte
+++ src/App.svelte
@@ -5,8 +5,12 @@
 
   let data = [1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
 </script>
+<div class="circle_container">
 
-<div class="mascara_circ"></div>
+  <div class="mascara_circ" style="width: 100px; height: 100px;"></div>
+  <div class="mascara_circ" style="width: 55px; height: 55px;"></div>
+  <div class="mascara_circ" style="width: 25px; height: 25px;"></div>
+</div>
 
 <!-- Estructura contenido HTML -->
 <h2>Ejemplo 1: Altura</h2>
@@ -29,17 +33,23 @@
 
 <!-- Estilos CSS -->
 <style>
+  .circle_container {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 10px;
+  }
   .mascara_circ {
-  width: 100px;
-  height: 100px;
-  border: 1px solid black;
-  background-image: url("https://raw.githubusercontent.com/visualizacion-de-datos-utdt/vd_d3_escalas/start/public/images/circles.svg");
-  background-repeat: no-repeat;
-  background-size: 100%;
-  border-radius: 50% ;
-  background-position: 0%;
-  /* border- */
-}
+    width: 100px;
+    height: 100px;
+    /* border: 1px solid black; */
+    background-image: url("https://raw.githubusercontent.com/visualizacion-de-datos-utdt/vd_d3_escalas/start/public/images/circles.svg");
+    background-repeat: no-repeat;
+    background-size: 100px;
+    border-radius: 50%;
+    background-position: center;
+    /* border- */
+  }
   /* Estilos CSS */
   .container,
   .container_2 {
